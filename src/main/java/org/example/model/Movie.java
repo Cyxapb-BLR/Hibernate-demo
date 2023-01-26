58910ea9bcb04ce51d025247a8dcf9976bff89f6
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-26 16:40:02 -0500

    HW52 - created Director and Movie

---
+++ src/main/java/org/example/model/Movie.java
@@ -0,0 +1,58 @@
+package org.example.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "movie")
+public class Movie {
+    @Id
+    @Column(name = "movie_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "year_of_prodaction")
+    private String year;
+
+    public Movie() {
+    }
+
+    public Movie(String name, String year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", year='" + year + '\'' +
+                '}';
+    }
+}
