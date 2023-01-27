8e748219e9ded1b9a692a3ea242fdfdee347c85d
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-27 17:24:41 -0500

    L57 - Lazy/Eager Fetch - cleared project for L

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,22 +1,16 @@
 package org.example;
 
-import org.example.model.Actor;
-import org.example.model.Movie;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Actor.class)
-                .addAnnotatedClass(Movie.class);
+        Configuration configuration = new Configuration().addAnnotatedClass()
+                .addAnnotatedClass();
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
 
@@ -24,13 +18,6 @@ public class App {
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-            Actor actor = session.get(Actor.class, 1);
-            System.out.println(actor.getMovies());
-
-            Movie movieToRemove = actor.getMovies().get(0);
-
-            actor.getMovies().remove(movieToRemove);
-            movieToRemove.getActors().remove(actor);
 
             session.getTransaction().commit();
         }
