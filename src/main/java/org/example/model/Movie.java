abcec51f2968ae4c1815297ea16fd5f263fe1d0e
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-27 14:05:15 -0500

    L56 - created Actor.java and Movie.java

---
+++ src/main/java/org/example/model/Movie.java
@@ -0,0 +1,56 @@
+package org.example.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Movie")
+public class Movie {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "year_of_production")
+    private int year;
+
+    public Movie() {
+    }
+
+    public Movie(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "name='" + name + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
