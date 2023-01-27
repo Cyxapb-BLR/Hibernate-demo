6df8cbf49599453d0ad8d68c8ae5156111f2932b
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-27 13:37:44 -0500

    L56 - ManyToMany in Hibernate - cleared project for L56

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import org.example.model.Passport;
-import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -11,25 +9,7 @@ import org.hibernate.cfg.Configuration;
  */
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class)
-                .addAnnotatedClass(Passport.class);
 
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
-        try {
-            session.beginTransaction();
-
-            Person person = new Person("Vad", 30);
-            Passport passport = new Passport(123456);
-
-            person.setPassport(passport);
-
-            session.save(person);
-
-            session.getTransaction().commit();
-        } finally {
-            sessionFactory.close();
-        }
 
     }
 }
